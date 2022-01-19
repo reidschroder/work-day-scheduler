0a23904abccf2ec01f00f2e9362633baa7c52f9e
Reid Schroder <reidschroder2@gmail.com>
2022-01-19 15:17:13 -0500

    added jQuery and local storage to make a functional scheduler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,37 @@
-var toDo = [];
+$(".saveBtn").click(function(){
+    var hour = $(this).siblings("textarea").attr("id")
+
+    var text = $(this).siblings("textarea").val()
+    
+    localStorage.setItem(hour, text)
+})
+
+$("textarea").each(function(){
+    var textareaHour = parseInt($(this).attr("id"))
+    var currentHour = moment().hours()
+    if (textareaHour === currentHour) {
+        $(this).addClass("present")
+    }
+    else if (textareaHour < currentHour) {
+        $(this).addClass("past")
+    }
+    else {
+        $(this).addClass("future")
+    }
+    
+})
+// call local storage to return on refresh
+
+$("#9").val(localStorage.getItem("9"))
+$("#10").val(localStorage.getItem("10"))
+$("#11").val(localStorage.getItem("11"))
+$("#12").val(localStorage.getItem("12"))
+$("#13").val(localStorage.getItem("13"))
+$("#14").val(localStorage.getItem("14"))
+$("#15").val(localStorage.getItem("15"))
+$("#16").val(localStorage.getItem("16"))
+$("#17").val(localStorage.getItem("17"))
 
 
-//Set local storage
-localStorage.setItem("toDo", JSON.stringify(toDo));
+
+
